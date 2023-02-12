58b58db5a6e0cb3a4c291454f8c1ae421e0d3f92
ITPMProject <itpmproject233@gmail.com>
2023-02-12 08:32:06 +0000

    project uploade

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [count, setCount] = useState("")
+
+  const fnCheckStatus = () => {
+    axios.get('http://localhost:5000/getStatus').then((res) => setCount(res.data)).catch((err) => alert(err))
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => fnCheckStatus()}>
+          Click Me
+        </button>
+        <p style={{ fontSize: '24px', fontWeight: 'bold' }}>
+          {count}
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
+
